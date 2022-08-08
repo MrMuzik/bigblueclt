05020fd1180da0fec706c76906cc7cd26b5a6bbc
Bryan Markham <muzik702@gmail.com>
2022-08-07 20:51:24 -0400

    testing CI

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header header">
         <img src={logo} className="App-logo" alt="logo" />
         <nav>
-          Edit <code>src/App.js</code> and save to reload. CI?
+          Edit <code>src/App.js</code> and save to reload.
         </nav>
+        <p>CI is working!</p>
         <Link to="/about">About</Link>
       </header>
     </div>
